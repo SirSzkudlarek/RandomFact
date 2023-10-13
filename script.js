026d8b41759c539154ad03c5a6e6a69f6c7fd094
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2023-10-14 01:35:55 +0200

    Task done

---
+++ script.js
@@ -0,0 +1,49 @@
+const currentDay = document.querySelector('.current-day')
+const funFact = document.querySelector('.fun-fact')
+
+const facts = [
+	'Krokodyl nie potrafi wystawić języka.',
+	'Każdy człowiek spędził około pół godziny jako pojedyncza komórka.',
+	'Dźwięk przemieszcza się 15 razy szybciej przez stal niż przez powietrze.',
+	'Leniwce potrzebują dwóch tygodni na strawienie jedzenia.',
+	'Goryle śpią nawet czternaście godzin dziennie.',
+	'Język kameleona jest dwukrotnie dłuższy od jego ciała.',
+	'Chińczycy w ciągu roku zużywają około 80 miliardów pałeczek.',
+	'Żeby wejść na Wieżę Eiffla trzeba pokonać aż 1710 stopni.'
+]
+
+let day;
+
+switch (new Date().getDay()) {
+	case 1:
+  day = "Poniedziałek";
+	break;
+
+	case 2:
+	day = "Wtorek";
+	break;
+	
+	case 3:
+	day = "Środa";
+	break;
+
+	case 4:
+	day = "Czwartek";
+	break;
+
+	case 5:
+	day = "Piątek";
+	break;
+
+	case 6:
+	day = "Sobota";
+	break;
+
+	case 7:
+	day = "Niedziela";
+}
+
+currentDay.textContent = day.toLowerCase();
+
+const fact = facts[Math.floor(Math.random()*facts.length)];
+funFact.textContent = fact;
